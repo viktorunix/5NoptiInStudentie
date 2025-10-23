f10ac1d5016c9a1661d24fe48c7a431e881d5725
Murgu Andrei <murgua03@gmail.com>
2025-10-23 18:53:30 +0300

    added video support ( and we will remove it lol cuz it runs slower than CFR)

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import pygame
+import cv2
 import sys
 
 from windows.MainMenu import renderMainMenu
@@ -25,15 +26,24 @@ sound = pygame.mixer.Sound("/mnt/c/Users/Andrei/PycharmProjects/5NoptiInStudenti
 channel = pygame.mixer.find_channel()
 channel.play(sound)
 
+cap = cv2.VideoCapture("/mnt/c/Users/Andrei/PycharmProjects/5NoptiInStudentie/assets/videos/mainmenu.mp4")
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    ret, frame = cap.read()
+    if not ret:
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        continue
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame = pygame.surfarray.make_surface(frame.swapaxes(0,1))
+    screen.blit(frame,(0,0))
     renderMainMenu(screen, clock, WIDTH, HEIGHT)
     if(not channel.get_busy()):
         channel.play(sound)
 
-
+    pygame.display.flip()
+    clock.tick(60)
+cap.release()
 pygame.quit()
 sys.exit()
