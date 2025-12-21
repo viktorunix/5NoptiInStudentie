7fa97ff0cb5504ecb148400e89f3b0ee22127b70
Andrei Murgu <murgua03@gmail.com>
2025-12-21 16:14:49 +0200

    added the skell for the main antagonist

--- src/mechanics/Big_bug.py
+++ src/mechanics/Big_bug.py
@@ -1,22 +1,57 @@
-import random as rd
+import random
+
+from mechanics.clock import clock
+from utils.camera_state import camera_state
+
 
 class BigBug:
-    __state = 0
-    __backward_probs = [0, 0.24, 0.5, 0.25]
-    __forward_probs = [0.12, 0.24, 0.05, 0.4]
-    def __init__(self):
-        self.__state = 0
-        for i in range(3):
-            self.__forward_probs[i] = 1 - self.__forward_probs[i]
-    def getstate(self):
-        return self.__state
-    def setstate(self, state):
-        self.__state = state
-    def change_state(self):
-        num = rd.random()
-        if num < self.__backward_probs[self.__state]:
-            self.__state -= 1
-            return
-        if num > self.__forward_probs[self.__state]:
-            self.__state += 1
-        return
+    def __init(self, name: str, start_pos: camera_state, ai_level: int):
+        self.name = name
+        self.location = start_pos
+        self.ai_level = ai_level
+        self.move_timer = 0
+        self.move_interval = 300  # 300 ticks or 5 seconds
+
+        # the path of the bug current_location->list of possible locations
+
+        self.path_network = {
+            camera_state.BATHROOM_HALLWAY: [camera_state.MAIN_HALLWAY_A],
+            camera_state.MAIN_HALLWAY_A: [
+                camera_state.STAIRWAY,
+                camera_state.MAIN_HALLWAY_B,
+            ],
+            camera_state.STAIRWAY: [camera_state.MAIN_HALLWAY_B],
+            camera_state.MAIN_HALLWAY_B: [camera_state.MAIN_HALLWAY_OFFICE],
+            camera_state.MAIN_HALLWAY_OFFICE: [camera_state.NONE],
+            camera_state.NONE: [],  # jumpscare here
+        }
+
+    def update(self):
+        """called once per frame in the game"""
+        self.move_timer += 1
+        if self.move_timer >= self.move_interval:
+            self.move_timer = 0
+            self.attempt_move()
+
+    def attempt_move(self):
+        """The main AI logic: rolls a 20 faced dice.
+        If the face value is less than the AI level then the bug will move"""
+        roll = random.randint(1, 20)
+
+        if roll <= self.ai_level:
+            self.move()
+
+    def move(self):
+        possible_moves = self.path_network.get(self.location, [])
+        if possible_moves:
+            next_room = random.choice(possible_moves)
+            self.location = next_room
+
+            if self.location == camera_state.NONE:
+                self.trigger_jumpscare()
+
+    def trigger_jumpscare(self):
+        print("JUMPSCARE")
+
+    def set_ai_level(self, level):
+        self.ai_level = level
